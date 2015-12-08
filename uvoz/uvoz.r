a395f53c65747e2b224608bf2f0613e6f039d348
Veronika Nabergoj <Veronika Nabergoj>
2015-12-08 16:14:28 +0100

    dodatni popravki

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -6,16 +6,16 @@
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
 
-##odpremo pakete:
+#odpremo pakete:
 library(dplyr)
 
 #1.tabela: PODATKI O PRESELJEVANJU - STAROSTNE SKUPINE:
 
-##poimenujemo stolpce:
+#poimenujemo stolpce:
 
 stolpci<-c("priseljeni_ali_odseljeni","starostna_skupina","leto","državljanstvo","skupaj","moški","ženske")
 
-##uvozimo podatke:
+#uvozimo podatke:
 
 uvozi<-function(){
   return(read.csv2(file="podatki/podatki-o-preseljevanju-starostneskupine",
@@ -27,22 +27,31 @@ uvozi<-function(){
 
 tabela<-uvozi()
 
-###uredimo prazna mesta:
-
-####funkcija: 
-uredi<-function(x,y,z){
-  tabela[x:(x+z), y] <- tabela[x, y]
-  return(tabela)
-}
+#uredimo prazna mesta:
 
 for (i in 2:1221){
   tabela[i,1]<-tabela[1,1]
 } 
+tabela[1223:2442,1]<-tabela[1222,1]
+ 
 
-for (i in 1223:2442){
-  tabela[i,1]<-tabela[1222,1]
-} 
+tabela[2:62, 2] <- tabela[2, 2]
 
-uredi(2,2,60)
+#funkcija:
+uredi<-function(x,y,z){
+  while(x<length(tabela[,y])){ 
+    tabela[x:(x+z),y]<-tabela[x,y]
+    x<-x+z+1
+  }
+  return(tabela)
+}
 
-uredi(63,2,60)
+#2. stolpec:
+tabela<-uredi(2,2,60)
+
+#3. stolpec:
+tabela<-uredi(3,3,2)
+
+
+
+
