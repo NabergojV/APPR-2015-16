830d8de8d01700b67cd1c2845c84e5d66aa46681
Veronika Nabergoj <Veronika Nabergoj>
2016-01-05 22:10:40 +0100

    dodano

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,11 +7,6 @@ zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
 druzine <- preuredi(druzine, zemljevid, "OB_UIME", c("Ankaran", "Mirna"))
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
-
 
 pretvori.zemljevid <- function(zemljevid) {
   fo <- fortify(zemljevid)
