89e1e478c2c1b810e9ea9761d77af3be63faab73
Veronika Nabergoj <Veronika Nabergoj>
2016-02-21 14:59:35 +0100

    napredna analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,4 +1,18 @@
 # 4. faza: Analiza podatkov
 
-barve <- rainbow(length(levels(obcine[[7]])))
-names(barve) <- levels(obcine[[7]])
+## Priseljeni, starostne skupine skupaj, državljanstvo skupaj, ločeno po spolu:
+
+priseljenimoski <- priseljeni %>% filter(starostna.skupina == "25-29 let") %>% 
+                    filter(državljanstvo=="Selitve - SKUPAJ")  %>%
+                    filter(spol=="Moški")
+
+g <- ggplot(priseljenimoski, aes(x=leto, y=stevilka)) + geom_point()
+print(g)
+
+h<- g + geom_smooth(method = "lm")
+print(h)
+
+kv <- lm(data =priseljenimoski , stevilka ~ leto + I(leto^2))
+kv <- g + geom_smooth(method = "lm", formula = y ~ x + I(x^2))
+print(kv)
+
