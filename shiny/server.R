9def7235dc050ba8e9a19c46f53e49d14c5cec4b
Veronika Nabergoj <Veronika Nabergoj>
2016-02-23 11:16:24 +0100

    shiny

--- shiny/server.R
+++ shiny/server.R
@@ -6,11 +6,12 @@ if ("server.R" %in% dir()) {
 
 shinyServer(function(input, output){
   output$grafiregije <- renderPlot({
-    title <- "Graf"
+    title <- "Graf za leto : "
     regijeleto <- razberi(input$izberi,"leto",tabela2)
-    ggplot(data=regijeleto,
-    aes(regija,input$izberi2))+ geom_bar(stat="identity",fill="deeppink3",size=10)+
-    coord_flip()
+    podatek <-input$izb
+    ggplot(data=regijeleto,aes_string("regija", podatek))+ 
+      geom_bar(stat="identity",fill="deeppink3",size=10)+
+      coord_flip()
 
   })
   
