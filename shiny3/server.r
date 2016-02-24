8db20710a25ca9a1c7b531d5ead02308c1e8b291
Veronika Nabergoj <Veronika Nabergoj>
2016-02-24 18:01:11 +0100

    shiny 3 zemljevid

--- shiny3/server.r
+++ shiny3/server.r
@@ -1,9 +1,35 @@
 library(shiny)
 
-if ("server3.r" %in% dir()) {
+if ("server.r" %in% dir()) {
   setwd("..")
 }
 
 shinyServer(function(input, output){
   
+  output$zemljevid <- renderPlot({
+    
+    letozem <- razberi(input$letoizberi,"leto",tabela2)
+    
+    rownames(letozem) <- letozem$regija
+    regleto <- preuredi(letozem, zemljevid, "NAME_1")
+    
+    zeml$priseljeni.skupaj <- regleto$Priseljeni.iz.tujine.skupaj
+    zeml$priseljeni.iz.tujine.na.1000.prebivalcev <-regleto$Priseljeni.iz.tujine.na.1000.prebivalcev
+    zeml$priseljeni.ženske <- regleto$Priseljeni.iz.tujine.ženske
+    zeml$priseljeni.moški <- regleto$Priseljeni.iz.tujine.moški
+    zeml$odseljeni.skupaj <- regleto$Odseljeni.v.tujino.skupaj
+    zeml$odseljeni.ženske <- regleto$Odseljeni.v.tujino.ženske
+    zeml$odseljeni.moški <- regleto$Odseljeni.v.tujino.moški
+    zeml$odseljeni.v.tujino.na.1000.prebivalcev <- regleto$Odseljeni.v.tujino.na.1000.prebivalcev
+    zeml$priseljeni.minus.odseljeni <-zeml$priseljeni.skupaj-zeml$odseljeni.skupaj
+    
+    končnipodatki <- pretvori.zemljevid(zeml)
+    
+    ggplot() + geom_polygon(data = končnipodatki, 
+                            aes(x=long, y=lat, group=group,
+                            fill=input$priods),
+                            color = "grey35") +
+                            scale_fill_gradient(low="violetred4", high="violet")
+ })
 })
+
--- shiny3/ui.r
+++ shiny3/ui.r
@@ -1,5 +1,23 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  sidebarLayout(
   
-))
+  sidebarPanel(
+    sliderInput(inputId ="letoizberi",label="Izberi leto:", value=1995, min=1995, max=2014),
+    
+    selectInput(inputId="priods",label="Izberi željeno:", 
+                choices = list("Priseljeni iz tujine skupaj" = "priseljeni.skupaj", 
+                               "Priseljeni iz tujine-ženske" = "priseljeni.ženske",
+                               "Priseljeni iz tujine-moški" = "priseljeni.moški",
+                               "Odseljeni v tujino skupaj"="odseljeni.skupaj" ,
+                               "Odseljeni v tujino-ženske"= "odseljeni.ženske",
+                               "Odseljeni v tujino-moški"="odseljeni.moški",
+                               "Prirast"="priseljeni.minus.odseljeni"),
+                                selected = "priseljeni.skupaj")),
+  mainPanel(
+    plotOutput(outputId="zemljevid")
+  )
+  
+)))
+
