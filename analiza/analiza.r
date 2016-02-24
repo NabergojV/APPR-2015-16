b87ce60e68f597cb2fdd29c79ca4180d78a92c86
Veronika Nabergoj <Veronika Nabergoj>
2016-02-24 18:01:41 +0100

    popravki

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -10,19 +10,19 @@ priseljenimoski <- priseljeni %>% filter(starostna.skupina == "25-29 let") %>%
                     filter(spol=="Moški")
 
 g <- ggplot(priseljenimoski, aes(x=leto, y=stevilka)) + geom_point()
-print(g)
+#print(g)
 
 z <- lowess(priseljenimoski$leto, priseljenimoski$stevilka)
 z <- g + geom_line(data=as.data.frame(z), aes(x=x, y=y), color="green")
-print(z)
+#print(z)
 
 mls <- loess(data = priseljenimoski, stevilka ~ leto)
 mls <- g + geom_smooth(method = "loess")
-print(mls)
+#print(mls)
 
 mgam <- gam(data = priseljenimoski, stevilka ~ s(leto))
 mgam <- g + geom_smooth(method = "gam", formula = y ~ s(x))
-print(mgam)
+#print(mgam)
 
 # Priseljeni, državljanstvo skupaj, po spolu, po letih:
 
@@ -31,13 +31,13 @@ priseljenispol <- prinapr %>% filter(starostna.skupina == "Starostne skupine - S
 
 h <- ggplot(priseljenispol, aes(x=zenske, y=moski,color=leto)) + geom_point(size=10)+ 
                             scale_color_distiller(palette = "RdPu")
-print(h)
+#print(h)
 
 mls2 <- loess(data = priseljenispol, moski ~ zenske)
 mls2<- h + geom_smooth(method = "loess",alpha=0.2)
-print(mls2)
+#print(mls2)
 
 
 mgam2 <- gam(data = priseljenispol, moski ~ s(zenske))
 mgam2 <- h + geom_smooth(method = "gam", formula = y ~ s(x),alpha=0.2)
-print(mgam2)
+#print(mgam2)
--- shiny/ui.R
+++ shiny/ui.R
@@ -8,13 +8,14 @@ shinyUI(fluidPage(
     sidebarPanel(
         sliderInput(inputId ="izberi",label="Izberi leto:", value=1995, min=1995, max=2014),
   
-        radioButtons(inputId="izb",label="Izberi željeno:", choices = list("Priseljeni iz tujine skupaj" = "Priseljeni.iz.tujine.skupaj", 
-                                                                               "Priseljeni iz tujine-ženske" = "Priseljeni.iz.tujine.ženske",
-                                                                               "Priseljeni iz tujine-moški" = "Priseljeni.iz.tujine.moški",
-                                                                               "Odseljeni v tujino skupaj"="Odseljeni.v.tujino.skupaj" ,
-                                                                               "Odseljeni v tujino-ženske"= "Odseljeni.v.tujino.ženske",
-                                                                               "Odseljeni v tujino-moški"="Odseljeni.v.tujino.moški" ),
-                                                                               selected = "Priseljeni.iz.tujine.skupaj")),
+        radioButtons(inputId="izb",label="Izberi željeno:", 
+                    choices = list("Priseljeni iz tujine skupaj" = "Priseljeni.iz.tujine.skupaj", 
+                                   "Priseljeni iz tujine-ženske" = "Priseljeni.iz.tujine.ženske",
+                                   "Priseljeni iz tujine-moški" = "Priseljeni.iz.tujine.moški",
+                                   "Odseljeni v tujino skupaj"="Odseljeni.v.tujino.skupaj" ,
+                                   "Odseljeni v tujino-ženske"= "Odseljeni.v.tujino.ženske",
+                                   "Odseljeni v tujino-moški"="Odseljeni.v.tujino.moški" ),
+                                    selected = "Priseljeni.iz.tujine.skupaj")),
   mainPanel(
   plotOutput(outputId="grafiregije")
   )
