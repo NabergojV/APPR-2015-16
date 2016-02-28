532c4114365323db706dec17d9105a620339837f
Veronika Nabergoj <Veronika Nabergoj>
2016-02-28 16:04:06 +0100

    popravek

--- shiny2/server.r
+++ shiny2/server.r
@@ -6,9 +6,12 @@ if ("server.r" %in% dir()) {
 
 shinyServer(function(input, output){
   output$podrzavljanstvu <- renderPlot({
+    
     prialiod<-filter(tabela,priseljeni.ali.odseljeni== input$Izbpris)
+    
     tabelca <-razberi(input$izbleto,"leto",prialiod)
-    ggplot(data=tabelca%>%filter(starostna.skupina !="Starostne skupine - SKUPAJ")%>%filter(spol!="Spol - SKUPAJ"),
+    
+    ggplot(data=tabelca %>% filter(starostna.skupina != "Starostne skupine - SKUPAJ") %>% filter(spol!="Spol - SKUPAJ"),
            aes(starostna.skupina,stevilka,fill=spol))+ geom_bar(stat="identity",size=3) + 
       coord_flip()+ facet_wrap(~ drzavljanstvo)
   })
